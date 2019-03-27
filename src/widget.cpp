aeeb7d94b33bd2b49495717becdcf1d3cb1b7365
justforlxz <justforlxz@gmail.com>
2019-03-27 13:23:05 +0800

    init

---
+++ src/widget.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2017 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     justforlxz <zhangdingyuan@deepin.com>
+ *
+ * Maintainer: justforlxz <zhangdingyuan@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "widget.h"
+
+Widget::Widget(QWidget *parent)
+    : DBlurEffectWidget(parent)
+{
+}
+
+Widget::~Widget()
+{
+
+}
---
+++ src/widget.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     justforlxz <zhangdingyuan@deepin.com>
+ *
+ * Maintainer: justforlxz <zhangdingyuan@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <DBlurEffectWidget>
+
+DWIDGET_USE_NAMESPACE
+
+class Widget : public DBlurEffectWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+};
+
+#endif // WIDGET_H
